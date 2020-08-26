0d09817f4c15b50815ea9eef254af9fbf7a55adf
William Harris <wharris@upscalews.com>
2020-08-25 22:27:39 -0700

    add doc for notification command

--- wsl-notification.sh
+++ wsl-notification.sh
@@ -1,6 +1,11 @@
 # https://codelearn.me/2019/01/13/wsl-windows-toast.html
 # https://github.com/Windos/BurntToast
 # https://stackoverflow.com/questions/2013547/assigning-default-values-to-shell-variables-with-a-single-command-in-bash
+# https://unix.stackexchange.com/questions/122845/using-a-b-for-variable-assignment-in-scripts/122878
+
+# https://unix.stackexchange.com/questions/232714/run-command-2-regardless-of-whether-command-1-succeeds-or-not
+# Usage: 
+# whatever-command.sh ; ~/my_confs/wsl-notification.sh
 
 DEFAULT_VALUE="task complete!"
 
