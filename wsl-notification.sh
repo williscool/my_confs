b3af295855c4389a01f4bbba4a8b64637441e924
William Harris <wharris@upscalews.com>
2021-01-20 03:34:10 -0500

    fix execution policy for wsl notification

--- wsl-notification.sh
+++ wsl-notification.sh
@@ -4,10 +4,14 @@
 # https://unix.stackexchange.com/questions/122845/using-a-b-for-variable-assignment-in-scripts/122878
 
 # https://unix.stackexchange.com/questions/232714/run-command-2-regardless-of-whether-command-1-succeeds-or-not
+
+
+# https://github.com/Windos/BurntToast/discussions/108
+
 # Usage: 
 # whatever-command.sh ; ~/my_confs/wsl-notification.sh
 
 DEFAULT_VALUE="task complete!"
 
 MSG=${1:-$DEFAULT_VALUE}
-powershell.exe 'New-BurntToastNotification -Text '\"$MSG\"' -Sound "Default" -SnoozeAndDismiss'
+powershell.exe -executionpolicy bypass -command 'New-BurntToastNotification -Text '\"$MSG\"' -Sound "Default" -SnoozeAndDismiss'
